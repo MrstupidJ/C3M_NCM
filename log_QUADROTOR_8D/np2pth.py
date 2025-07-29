6360c5a2585ae85c0c6d731c2a98538834dca6c9
MrstupidJ <johnnycheng390@gmail.com>
2025-07-29 16:04:22 +1000

    modify deprecated code

--- log_QUADROTOR_8D/np2pth.py
+++ log_QUADROTOR_8D/np2pth.py
@@ -4,8 +4,7 @@ import torch
 import numpy as np
 
 def get_controller_wrapper(controller_path):
-    # _controller = torch.load(controller_path, map_location=torch.device('cpu'))
-    _controller = torch.load(controller_path, map_location=torch.device('cpu'), weights_only=False)
+    _controller = torch.load(controller_path, map_location=torch.device('cpu'))
     _controller.cpu()
 
     def controller(x, xe, uref):
